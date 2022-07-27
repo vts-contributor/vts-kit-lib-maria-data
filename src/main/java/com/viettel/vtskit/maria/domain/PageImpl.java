e3b378932a0f47389aeadd8be30253fb1793826b
phuhk <phuhk@viettel.com.vn>
2022-07-27 17:36:43 +0700

    Update PageImpl.java

--- src/main/java/com/viettel/vtskit/maria/domain/PageImpl.java
+++ src/main/java/com/viettel/vtskit/maria/domain/PageImpl.java
@@ -12,7 +12,7 @@ import java.util.function.Function;
 
 public class PageImpl<T> implements Page<T> {
     private final long total;
-    private final List<T> content = new ArrayList();
+    private final List<T> content = new ArrayList<T>();
     private final Pageable pageable;
 
     public PageImpl(List<T> content, Pageable pageable, long total) {
@@ -21,18 +21,22 @@ public class PageImpl<T> implements Page<T> {
         this.pageable = pageable;
     }
 
+    @Override
     public boolean isFirst() {
         return !this.hasPrevious();
     }
 
+    @Override
     public boolean hasPrevious() {
         return this.getNumber() > 0;
     }
 
+    @Override
     public Pageable nextPageable() {
         return this.hasNext() ? this.pageable.next() : Pageable.unpaged();
     }
 
+    @Override
     public Pageable previousPageable() {
         return this.hasPrevious() ? this.pageable.previousOrFirst() : Pageable.unpaged();
     }
@@ -42,50 +46,62 @@ public class PageImpl<T> implements Page<T> {
         return null;
     }
 
+    @Override
     public boolean hasContent() {
         return !this.content.isEmpty();
     }
 
+    @Override
     public List<T> getContent() {
         return Collections.unmodifiableList(this.content);
     }
 
+    @Override
     public Pageable getPageable() {
         return this.pageable;
     }
 
+    @Override
     public Sort getSort() {
         return this.pageable.getSort();
     }
 
+    @Override
     public Iterator<T> iterator() {
         return this.content.iterator();
     }
 
+    @Override
     public int getSize() {
         return this.pageable.isPaged() ? this.pageable.getPageSize() : this.content.size();
     }
 
+    @Override
     public int getNumberOfElements() {
         return this.content.size();
     }
 
+    @Override
     public int getTotalPages() {
         return this.getSize() == 0 ? 1 : (int)Math.ceil((double)this.total / (double)this.getSize());
     }
 
+    @Override
     public long getTotalElements() {
         return this.total;
     }
 
+    @Override
     public boolean hasNext() {
         return this.getNumber() + 1 < this.getTotalPages();
     }
 
+    @Override
     public boolean isLast() {
         return !this.hasNext();
     }
 
+    @Override
     public int getNumber() {
         return this.pageable.isPaged() ? this.pageable.getPageNumber() : 0;
     }
